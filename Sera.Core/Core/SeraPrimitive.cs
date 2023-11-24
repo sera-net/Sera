5cd89ea71bbe7d56b4114bbdfd0b95ab591aa37d
U2A5F <U2A5F@outlook.com>
2023-11-24 19:53:50 +0800

    Any

---
+++ Sera.Core/Core/SeraPrimitive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Sera.TaggedUnion;
+using Sera.Utils;
+
+namespace Sera.Core;
+
+[Union(ExternalTags = true, ExternalTagsName = "SeraPrimitiveTypes")]
+public readonly partial struct SeraPrimitive
+{
+    [UnionTemplate]
+    private interface Template
+    {
+        bool Boolean();
+        sbyte SByte();
+        byte Byte();
+        short Int16();
+        ushort UInt16();
+        int Int32();
+        uint UInt32();
+        long Int64();
+        ulong UInt64();
+        Int128 Int128();
+        UInt128 UInt128();
+        nint IntPtr();
+        nuint UIntPtr();
+        Half Half();
+        float Single();
+        double Double();
+        decimal Decimal();
+        NFloat NFloat();
+        Box<BigInteger> BigInteger();
+        Complex Complex();
+        TimeSpan TimeSpan();
+        DateOnly DateOnly();
+        TimeOnly TimeOnly();
+        DateTime DateTime();
+        DateTimeOffset DateTimeOffset();
+        Guid Guid();
+        Range Range();
+        Index Index();
+        char Char();
+        Rune Rune();
+        Uri Uri();
+        Version Version();
+    }
+}
--- Sera.Core/Utils/Box.cs
+++ Sera.Core/Utils/Box.cs
@@ -1,13 +1,68 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Sera.Utils;
 
-public class Box<T>(T value) : IRef<T>
+public class Box<T>(T Value) : IRef<T>, IEquatable<T>, IEquatable<Box<T>>, IComparable<T>, IComparable<Box<T>>
 {
-    public T Value = value;
+    public T Value = Value;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ref T GetRef() => ref Value;
+
+    #region ToString
+
+    public override string ToString() => $"{Value}";
+
+    #endregion
+
+    #region Equals
+
+    public bool Equals(T? other) => EqualityComparer<T>.Default.Equals(Value, other);
+
+    public bool Equals(Box<T>? other) => ReferenceEquals(other, null) && Equals(other!.Value);
+
+    public override bool Equals(object? obj) => obj is Box<T> box ? Equals(box) : obj is T v && Equals(v);
+
+    // ReSharper disable once NonReadonlyMemberInGetHashCode
+    public override int GetHashCode() => Value?.GetHashCode() ?? 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Box<T> left, Box<T> right) => left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Box<T> left, Box<T> right) => !left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Box<T> left, T right) => left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Box<T> left, T right) => !left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(T left, Box<T> right) => right.Equals(left);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(T left, Box<T> right) => !right.Equals(left);
+
+    public int CompareTo(T? other) => Comparer<T>.Default.Compare(Value, other);
+
+    public int CompareTo(Box<T>? other) => ReferenceEquals(other, null) ? -1 : CompareTo(other.Value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator <(Box<T> left, Box<T> right) => left.CompareTo(right) < 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator >(Box<T> left, Box<T> right) => left.CompareTo(right) > 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator <=(Box<T> left, Box<T> right) => left.CompareTo(right) <= 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator >=(Box<T> left, Box<T> right) => left.CompareTo(right) >= 0;
+
+    #endregion
 }
 
 public interface IRef<T>
